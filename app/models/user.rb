7035c2f8226c398c122e0393b03200fb5aaaa56f
Edemone <edem.agbenyo@gmail.com>
2019-12-03 17:21:43 +0000

    Implement for submission using plain form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validate :name presence: true
-  validate :email presence: true
-  validate :password presence: true
+  validates :username, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
 end
