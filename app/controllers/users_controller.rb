e13203b208ce8d28fb749ebace54d73de6e9ecab
Edemone <edem.agbenyo@gmail.com>
2019-12-03 20:10:11 +0000

    updating of user implementation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,18 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+   @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to edit_user_path
+    else
+      render :new
+    end
+  end
   private
 
   def user_params
